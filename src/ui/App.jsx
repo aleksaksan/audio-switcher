778c179ce4e2bb47a57f99def69597468db0a626
Aleksan <sanyok.10rus@gmail.com>
2025-05-23 03:18:44 +0300

    fixed stating Server

--- src/ui/App.jsx
+++ src/ui/App.jsx
@@ -1,9 +1,28 @@
 import './App.css';
 import { HashRouter  } from 'react-router';
 import { Router } from './components/Router';
+import { PORT, useServerStore } from './store/serverStore';
+import { useEffect } from 'react';
 
 function App() {
+  const { toggleServer, checkServerStatus } = useServerStore();
 
+  useEffect(() => {
+    checkServerStatus();
+  }, [checkServerStatus]);
+  
+  useEffect(() => {
+    const port = localStorage.getItem(PORT);
+    if (port) {
+      window.electron.isServerRunning().then(running => {
+        if (!running) {
+          toggleServer();
+          console.log('server started')
+        }
+      });
+    }
+  }, []); 
+    
   return (
     <HashRouter >
       <Router />
